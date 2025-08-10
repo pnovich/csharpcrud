442d33d9bdb571aafaf5ff19f77bd10a90503ec4
Pavlo <pppp_84@ukr.net>
2025-08-10 04:14:17 +0300

    build cleanup

--- ExampleApi/DTOs/ExampleDto.cs
+++ ExampleApi/DTOs/ExampleDto.cs
@@ -2,6 +2,7 @@ namespace MyApp.DTOs
 {
     public class ExampleDto
     {
+        public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public int? Number { get; set; }
     }
